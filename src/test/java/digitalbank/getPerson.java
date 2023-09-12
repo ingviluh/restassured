0211ac959c76285711cd370d55bfbfaa935cdbbe
Magnus Tenmann <magnus.tenmann@fremtind.no>
2023-09-12 20:54:45 +0200

    Added dependencies

--- src/test/java/digitalbank/getPerson.java
+++ src/test/java/digitalbank/getPerson.java
@@ -1,10 +1,16 @@
 package digitalbank;
 
 import org.junit.BeforeClass;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
+
+import static io.restassured.RestAssured.given;
+
 
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
@@ -21,7 +27,15 @@ public class getPerson {
         enableLoggingOfRequestAndResponseIfValidationFails();
     }
 
-
-
-
+    @Test
+    @Parameters("id")
+    public void getCustomerName(@Optional("79") String id){
+        given()
+                .when()
+                .get("/account/" + id)
+                .then()
+                .body(containsString("id"))
+                .body("id", equalTo(id))
+                .statusCode(200);
+    }
 }
